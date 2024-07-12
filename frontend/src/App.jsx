ce3124cae1d06ee24237a7f233d4a5d6d7328bd4
Aritra Ray <aritraray120@gmail.com>
2024-07-12 12:24:38 +0530

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import Cart from "./pages/Cart/Cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import Home from "./pages/Home/Home";
+
 
 function App() {
   return ( 
     <div className='app'>
 
       <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/order' element={<PlaceOrder />} />
+      </Routes>
 
 
     </div>
