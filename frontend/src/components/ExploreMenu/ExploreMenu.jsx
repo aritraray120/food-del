f3e61655af3b0571a88e77c8d5464e0ed336b948
Aritra Ray <aritraray120@gmail.com>
2024-07-21 13:57:59 +0530

    Update

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -14,7 +14,7 @@ function ExploreMenu({category, setCategory}) {
                 {menu_list.map((item, index) => {
                     
                     return (
-                        <div onClick={()=>setCategory(prev => prev === item.menu_name?"All":item.menu_name)} key={index}className='explore-menu-list-item'>
+                        <div onClick={()=>setCategory(prev => prev === item.menu_name?"All":item.menu_name)} key={index} className='explore-menu-list-item'>
 
                             <img className={category === item.menu_name?"active":""} src={item.menu_image} alt='' />
                             <p>{item.menu_name}</p>
