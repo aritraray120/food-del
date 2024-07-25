c73cb32455d511f75d4d8e21504eef5dca5834a0
Aritra Ray <aritraray120@gmail.com>
2024-07-26 01:20:12 +0530

    U

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,10 +1,12 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
 
 
 export const StoreContext = createContext(null);
 
 function StoreContextProvider(props) {
+
+    const [cartItems, setCartItems] = useState({});
 
     const contextValue = {
 
