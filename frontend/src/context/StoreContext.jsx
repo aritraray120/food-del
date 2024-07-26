79f70770fd0e69be031da21745377f1526f22e53
Aritra Ray <aritraray120@gmail.com>
2024-07-26 12:27:53 +0530

    U

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -8,9 +8,28 @@ function StoreContextProvider(props) {
 
     const [cartItems, setCartItems] = useState({});
 
-    const contextValue = {
+    function addToCart(itemId) {
 
-        food_list
+        if(!cartItems[itemId]) {
+            setCartItems((prev)=>({...prev, [itemId]:1}));
+        }
+        else {
+            setCartItems((prev)=>({...prev, [itemId]:prev[itemId]+1}));
+        }
+
+    }
+
+    function removeFromCart(itemId) {
+        setCartItems((prev)=>({...prev, [itemId]:prev[itemId]-1}));
+    }
+
+    const contextValue = { 
+
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart
 
 
     }
