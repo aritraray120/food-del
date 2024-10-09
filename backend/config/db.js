55b51de26bf5d3d40b3c24b4d27edbbd98bf5de5
Aritra Ray <aritraray120@gmail.com>
2024-10-10 00:45:25 +0530

    u

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://aritraray120:kswA1bCSA2gzMOQ3@cluster0.mhyrz.mongodb.net/food-del')
+    await mongoose.connect('mongodb://localhost:27017/food-del')
         .then(() => console.log("DB connected")
-        )
-
+        ).catch (error => console.log(error.message)) 
+            
+        
 }
+
+// mongodb+srv://aritraray120:kswA1bCSA2gzMOQ3@cluster0.mhyrz.mongodb.net/food-del
 
