3ffd22f9691cee50c776bec809aa180f57104c21
Aritra Ray <aritraray120@gmail.com>
2024-11-30 01:21:08 +0530

    u

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,7 +1,59 @@
 import "./Add.css";
+import { assets } from "../../assets/assets";
 
 function Add() {
-    return ( <div>add</div> );
+    return ( 
+    
+    <div className="add">
+        
+        <form className="flex-col">
+
+            <div className="add-img-upload flex-col">
+                <p>Upload Image</p>
+                <label htmlFor="image">
+                    <img src={assets.upload_area} alt="" />
+                </label>
+                <input type="file" id="image" hidden required />
+            </div>
+
+            <div className="add-product-name flex-col">
+                <p>Product name</p>
+                <input type="text" name="name" placeholder="Type here" />
+            </div>
+
+            <div className="add-product-description flex-col">
+                <p>Product description</p>
+                <textarea name="description" rows="6" placeholder="Write content here" required></textarea>
+            </div>
+
+            <div className="add-category-price">
+                <div className="add-category flex-col">
+                    <p>Product category</p>
+                    <select name="category">
+                        <option value="Salad">Salad</option>
+                        <option value="Rolls">Rolls</option>
+                        <option value="Desserts">Desserts</option>
+                        <option value="Sandwich">Sandwich</option>
+                        <option value="Cake">Cake</option>
+                        <option value="Pure Veg">Pure Veg</option>
+                        <option value="Pasta">Pasta</option>
+                        <option value="Noodles">Noodles</option>
+                    </select>
+                </div>
+
+                <div className="ad-price flex-col">
+                    <p>Product price</p>
+                    <input type="number" name="price" placeholder="$20" />
+                </div>
+            </div>
+
+            <button type="submit" className="add-btn">ADD</button>
+
+        </form>
+
+    </div> 
+
+    );
 }
 
 export default Add;
